850056a907db5feb7045f55951baabcbb0174307
Süge Ákos <sugeakos@gmail.com>
2021-06-07 20:27:34 +0200

    house-manage-form

--- backend_php/login.php
+++ backend_php/login.php
@@ -24,36 +24,39 @@ if(isset($_POST['button'])){
                 setcookie("member_password", $password, time() + (86400 * 30), "/");
                 setcookie("name", $row["name"], time() + (86400 * 30), "/");
                 setcookie("lastname", $row["lname"], time() + (86400 * 30), "/");
-               // session_set_cookie_params("member_login", $email, time()+3600,'/');
+               	//session_set_cookie_params("member_login", $email, time()+3600,'/');
                 //session_set_cookie_params("member_password", $password, time()+3600,'/');
-/*
+
                 var_dump( $row['ID'] );
                 echo "<br>";
                 var_dump($_SERVER["REMOTE_ADDR"]);
                 echo "<br>";
                 var_dump($_SERVER["HTTP_USER_AGENT"]);
-*/
-                $insert_into_login = "INSERT INTO login(LoginID,IP, Browser,Time) VALUES ("
-                . strval($row['ID']) .
-                ","
-                .strval($_SERVER["REMOTE_ADDR"]) .
-                " ,"
-                . strval($_SERVER["HTTP_USER_AGENT"]) . 
-                " ,NOW())";
 
+               	$insert_into_login = "INSERT INTO login(LoginID,IP, Browser,Time) VALUES ("
+                . strval($row['ID']) .
+                ",'"
+                . strval($_SERVER["REMOTE_ADDR"]) .
+                "' ,'"
+                . strval($_SERVER["HTTP_USER_AGENT"]) . 
+                "' ,NOW())";
+            	echo "<br><br>";
+				var_dump($insert_into_login);
                 $insert_into_query = $conn-> prepare($insert_into_login);
                 $insert_into_query -> execute();
                 $lastInsertID = $conn -> lastInsertID();
 
                 if($lastInsertID > 0){
                     $success_message = "Sikeres adatbevitel";
-                    //header('Location: ../login.html');
+                	//echo $success_message;
+                    header('Location: /index.php');
                 }
                 else{
                     $success_message = "Nem sikerult az adatbevitel";
                     
                 }
                 //header('Location: ../index.php');
+            	//echo $success_message;
             }
             else{
                 goto a;
@@ -62,7 +65,7 @@ if(isset($_POST['button'])){
     }
     else{
         a:
-        header("Location: ../login.php");
+        header("Location: login.html");
     }
 }
 
