edc136c884614b54c5c347f7152748d5d33ae71f
Süge Ákos <sugeakos@gmail.com>
2021-06-10 23:03:26 +0200

    email verification form

--- Registered-user/index.php
+++ Registered-user/index.php
@@ -239,7 +239,7 @@ session_start();
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>
-                                <a class="dropdown-item" href="#">
+                                <a class="dropdown-item" href="profile-settings.php">
                                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Settings
                                 </a>
